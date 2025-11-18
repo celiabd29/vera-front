1b812db30b9370c345e3ad0014f67f7660eb6ec1
Eléa <elea.ya.pro@gmail.com>
2025-11-18 22:15:20 +0100

    Projet Angular routes

---
+++ angular/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { AboutComponent } from './pages/about/about';
+
+// routes, les navigations
+export const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: '**', redirectTo: '' }
+];
