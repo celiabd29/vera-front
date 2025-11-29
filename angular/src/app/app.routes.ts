d2599903fd7378f80e1324b04d36fb5b62758c2c
celiabd29 <c_abbad@stu-digital-campus.fr>
2025-11-29 15:13:00 +0100

    deploiement version unique

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,17 +2,18 @@
 
 // export const routes: Routes = [];
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil';
 import { HomeComponent } from './pages/home/home';
 import { AboutComponent } from './pages/about/about';
-import { FormComponent } from './pages/form/form';
+import { IndexComponent } from './pages/form/form';
 import { AuthGuard } from '../guard';
-import { AccueilComponent } from './pages/accueil/accueil';
+
 
 // routes, les navigations
 export const routes: Routes = [
   { path: '', component: AccueilComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard]},
-  { path: 'form', component: FormComponent, canActivate: [AuthGuard]},
+  { path: 'form', component: IndexComponent, canActivate: [AuthGuard]},
   { path: '**', redirectTo: '' }
 ];
