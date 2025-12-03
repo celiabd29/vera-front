0387db9cf2712b93c7e9cb34fcebd6acae08c073
celiabd29 <c_abbad@stu-digital-campus.fr>
2025-12-04 00:01:24 +0100

    recup footer + header

--- angular/src/app/shared/vera-header/vera-header.component.ts
+++ angular/src/app/shared/vera-header/vera-header.component.ts
@@ -1,8 +1,6 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { DarkModeService } from '../../services/darkmode.service';
 
 @Component({
   selector: 'app-vera-header',
@@ -10,21 +8,4 @@ import { DarkModeService } from '../../services/darkmode.service';
   templateUrl: './vera-header-chat.component.html',
   imports: [CommonModule, RouterModule],
 })
-export class VeraHeaderComponent implements OnInit {
-  logoPath = 'assets/logo-vera-noir.png';
-
-  constructor(private darkModeService: DarkModeService) {}
-
-  ngOnInit() {
-    this.darkModeService.darkMode$.subscribe(enabled => {
-      this.logoPath = enabled ? 'assets/logo-vera-blanc.png' : 'assets/logo-vera-noir.png';
-    });
-  }
-
-  toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-theme');
-    const enabled = body.classList.contains('dark-theme');
-    this.darkModeService.setDarkMode(enabled);
-  }
-}
+export class VeraHeaderComponent {}
